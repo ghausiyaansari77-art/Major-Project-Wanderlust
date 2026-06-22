7cb3d535425ae9ace7a3b936052d7ece796bbdb1
ghausiya Ansari <ghausiyaansari77@gmail.com>
2026-06-22 07:53:26 +0530

    Added home route

--- app.js
+++ app.js
@@ -94,8 +94,9 @@ app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
 app.get("/", (req, res) =>{
-    res.redirect("listings");
+    res.redirect("/listings");
 });
+
 app.all("/*splat", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
 });
